4606778157036ed7b7539dbddc9299f71757e6c9
C3NZ <cenz@cenz.io>
2018-11-26 10:15:36 -0800

    [fix] add comments and export router

--- routes/class.js
+++ routes/class.js
@@ -1,18 +1,23 @@
+//express imports
 const express = require('express');
 const router = express.Router();
 
-//
+//db imports
 const mongoose = require('mongoose');
 const Class = require('../models/Class')
 
+//Route for creating a class
 router.post('/', (req, res) => {
     Class.create(req.body).then(newClass => {
         res.redirect('/dashboard');
     })
 })
 
+//Router for deleting a class
 router.delete('/:classId', (req, res) => {
     Class.deleteOne({_id: req.params.classId}).then(deletedClass => {
         res.redirect('')
     })
 })
+
+module.exports = router;
