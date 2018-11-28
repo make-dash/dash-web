3cb4f009d9fb9317e530094319d87c8b8e825abb
C3NZ <cenz@cenz.io>
2018-11-28 00:41:50 -0800

    [fix] add testing and some extra routes for CRUD functionality

--- routes/class.js
+++ routes/class.js
@@ -6,24 +6,32 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Class = require('../models/Class')
 
+const studentsRouter = require('./students');
+
 //Route for viewing all classes
 router.get('/', (req, res) => {
     Class.find().then(classes => {
         res.render('classes', {classes: classes})
+    }).catch(err => {
+        console.error(err) 
     })
 })
 
 //Route for going to a specific class
 router.get('/:classId', (req, res) => {
-    Class.find({_id: req.params.classId}).populate('assignments').then(targetedClass => {
+    Class.find({_id: req.params.classId}).populate('assignments').populate('students').then(targetedClass => {
         res.render('class', {class: targetedClass});
+    }).catch(err => {
+        console.error(err)
     })
 })
 
 //Route for updating information about a class
 router.put('/:classId', (req, res) => {
-    Class.findOneAndUpdate({_id: req.params.classId}, req.body).populate('assignments').then(updatedClass => {
+    Class.findOneAndUpdate({_id: req.params.classId}, req.body).populate('assignments').populate('students').then(updatedClass => {
         res.render('class', {class: updatedClass})
+    }).catch(err => {
+        console.error(err)
     })
 })
 
@@ -31,6 +39,8 @@ router.put('/:classId', (req, res) => {
 router.post('/', (req, res) => {
     Class.create(req.body).then(newClass => {
         res.redirect('/dashboard');
+    }).catch(err => {
+        console.error(err)
     })
 })
 
@@ -38,6 +48,17 @@ router.post('/', (req, res) => {
 router.delete('/:classId', (req, res) => {
     Class.deleteOne({_id: req.params.classId}).then(deletedClass => {
         res.redirect('')
+    }).catch(err => {
+        console.error(err)
+    })
+})
+
+//Route for getting students that belong to a specific class
+router.get('/:classId/students', (req, res) => {
+    Class.findOne({_id: req.params.studentId}).populate('students').then(currentClass => {
+        res.json(currentClass);
+    }).catch(err => {
+        console.error(err)
     })
 })
 
