e0161554369ebd0d0d33983e71bdb803d3d30ea1
C3NZ <cenz@cenz.io>
2018-11-27 21:15:01 -0800

    [fix] point the dashboard router to the index route

--- app.js
+++ app.js
@@ -30,8 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Configure our routes
-app.use('/', indexRouter);
-app.use('/dashboard', dashboardRouter);
+app.use('/', dashboardRouter);
 app.use('/class', classRouter)
 
 // catch 404 and forward to error handler
