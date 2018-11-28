72d0b6a8ef056a1cece1da809a8c176b8ce27405
C3NZ <cenz@cenz.io>
2018-11-27 20:37:28 -0800

    [feat] add update and 2 get routes for full CRUD functionallity on classes

--- routes/class.js
+++ routes/class.js
@@ -6,6 +6,27 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const Class = require('../models/Class')
 
+//Route for viewing all classes
+router.get('/', (req, res) => {
+    Class.find().then(classes => {
+        res.render('classes', {classes: classes})
+    })
+})
+
+//Route for going to a specific class
+router.get('/:classId', (req, res) => {
+    Class.find({_id: req.params.classId}).then(targetedClass => {
+        res.render('class', {class: targetedClass});
+    })
+})
+
+//Route for updating information about a class
+router.put('/:classId', (req, res) => {
+    Class.findOneAndUpdate({_id: req.params.classId}, req.body).then(updatedClass => {
+        res.render('class', {class: updatedClass})
+    })
+})
+
 //Route for creating a class
 router.post('/', (req, res) => {
     Class.create(req.body).then(newClass => {
